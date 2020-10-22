be80fe1ce8143ea942dbd5eb71aceafb548bf31d
Dror Tirosh <dror@opengsn.org>
2020-10-22 18:07:29 +0300

    cleanup

--- functions/logproxy/logproxy.js
+++ functions/logproxy/logproxy.js
@@ -21,48 +21,18 @@ exports.handler = async (event, context) => {
         }
         ret = await fetch( loggerUrl, {
             method: event.httpMethod,
-            body: event.body,
-            headers
+            headers,
+            body: event.body
         })
-        console.log( 'resp=', ret)
         return {
             statusCode: 200,
             headers,
             body: JSON.stringify(ret)
         }
     } catch(e) {
-        console.log( 'ex=', e)
         return {
             statusCode: 400,
             body: "exception: "+e
         }
     }
-/*
-    await fetch('https://httpbin.org/post', {
-        method: 'post',
-        body:    JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
-    } catch (e) {
-        return {
-            statusCode: 200, headers, body: JSON.stringify({
-                success: false,
-                input: event,
-                error: e.toString(),
-                stack: e.stack.split('\n')
-            })
-        }
-    }
-    /*
-    {
-        "path": "Path parameter",
-        "httpMethod": "Incoming request's method name"
-        "headers": {Incoming request headers}
-        "queryStringParameters": {query string parameters }
-        "body": "A JSON string of the request payload."
-        "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
-    }
-    */
 }
